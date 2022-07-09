09aae29314a8cb99d392e0abf43d4668836f0a8c
Eyoubt <eyobtesfaye20@yahoo.com>
2022-07-09 09:08:31 -0700

    Updates

---
+++ precache-manifest.0eba690a8082730cde0847235d8f845c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb81b93393e7ba82be8b0eaf213f7ecc",
+    "url": "/Apple-replica-website/index.html"
+  },
+  {
+    "revision": "a398ae3f187c4f01bca4",
+    "url": "/Apple-replica-website/static/css/main.0b3e1432.chunk.css"
+  },
+  {
+    "revision": "3ded766dead9d74a187f",
+    "url": "/Apple-replica-website/static/js/2.d2aafb31.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/Apple-replica-website/static/js/2.d2aafb31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a398ae3f187c4f01bca4",
+    "url": "/Apple-replica-website/static/js/main.fa37adbd.chunk.js"
+  },
+  {
+    "revision": "683f2f187d535da23589",
+    "url": "/Apple-replica-website/static/js/runtime-main.23641358.js"
+  },
+  {
+    "revision": "1cd845d6f5a1445d85db5f4f31baf865",
+    "url": "/Apple-replica-website/static/media/Arcon-lg.1cd845d6.jpg"
+  },
+  {
+    "revision": "6876fc6527efcaa8ae1386693dce73d5",
+    "url": "/Apple-replica-website/static/media/Card_large.6876fc65.jpg"
+  },
+  {
+    "revision": "45b2503e8bb62934357a42699c1f33fc",
+    "url": "/Apple-replica-website/static/media/Card_small.45b2503e.jpg"
+  },
+  {
+    "revision": "5465cc591da2ac562ae16bb33f3575b8",
+    "url": "/Apple-replica-website/static/media/apple.5465cc59.svg"
+  },
+  {
+    "revision": "aea2d7189d99437b77d399f64935c8f3",
+    "url": "/Apple-replica-website/static/media/bag.aea2d718.svg"
+  },
+  {
+    "revision": "d34f65aa590c8d3a0928fb07f2d1289b",
+    "url": "/Apple-replica-website/static/media/central_park_lg.d34f65aa.jpg"
+  },
+  {
+    "revision": "edf94f2ff598948e0d9fdb0611b2cf0c",
+    "url": "/Apple-replica-website/static/media/central_park_sm.edf94f2f.jpg"
+  },
+  {
+    "revision": "a7ef5f4882048c96dcefc8239b534c8e",
+    "url": "/Apple-replica-website/static/media/iphone11-colored.a7ef5f48.jpg"
+  },
+  {
+    "revision": "573a6b368484e06a1543fd0af4d70bfa",
+    "url": "/Apple-replica-website/static/media/iphone11-pro-bg.573a6b36.jpg"
+  },
+  {
+    "revision": "30b06e91d5c36c592794c16ca7e42ae7",
+    "url": "/Apple-replica-website/static/media/ipodPronew-sm.30b06e91.jpg"
+  },
+  {
+    "revision": "f7f2ab1f61fd401300c25a2df3be6893",
+    "url": "/Apple-replica-website/static/media/ipodPronew.f7f2ab1f.jpg"
+  },
+  {
+    "revision": "851443b3e3a4a5ea65c4e33622cc4c42",
+    "url": "/Apple-replica-website/static/media/macbookair-new-sm.851443b3.jpg"
+  },
+  {
+    "revision": "890634a4409ae22865fbad51d6004953",
+    "url": "/Apple-replica-website/static/media/macbookair-new.890634a4.jpg"
+  },
+  {
+    "revision": "8dd1210f9d7be403307a214472c87f03",
+    "url": "/Apple-replica-website/static/media/search.8dd1210f.svg"
+  },
+  {
+    "revision": "3dda7621799aa621c3d6df229700440e",
+    "url": "/Apple-replica-website/static/media/spyder.3dda7621.jpg"
+  },
+  {
+    "revision": "aa3dc7331ab40ebb0b984342d3edb186",
+    "url": "/Apple-replica-website/static/media/watch.aa3dc733.jpg"
+  }
+]);
